c2b691191f23f0837dce0a5d918cc02543304fc4
Abdalrhman Saad <abdalrhmansaad24@gmail.com>
2024-08-15 09:35:23 +0300

    [283] [Time Beats: 43.52%] [Memory Beats: 15.83%] - LeetPush

---
+++ 283-Move-Zeroes.cs
@@ -0,0 +1,18 @@
+public class Solution
+{
+    public void MoveZeroes(int [] nums)
+    {
+        int zero = 0;
+        for (int i = 0; i < nums.Length; i++)
+        {
+            if (nums[i] != 0)
+            {
+                nums[zero++] = nums[i];
+            }
+            
+        }
+
+
+        for(int i = zero; i < nums.Length; i++) { nums[i] = 0; }
+    }
+}

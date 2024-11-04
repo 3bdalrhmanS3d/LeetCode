7b4a365cc1dd7ca3cec5cfc3bb416329d691c31f
Abdalrhman Saad <abdalrhmansaad24@gmail.com>
2024-11-04 15:28:44 +0200

    [1598] [Time Beats: 100.00%] [Memory Beats: 87.57%] - LeetPush

---
+++ 1598-Crawler-Log-Folder.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    int minOperations(vector<string>& logs) {
+
+        int cnt =0 ;
+        string fol ="";
+        for(int i =0 ; i < logs.size() ; i++ )
+        {
+            fol = logs[i] ;
+            
+            if( fol == "../")
+            {
+                if(cnt > 0)
+                {
+                    cnt--;
+                }
+                else
+                {
+                    cnt = cnt ;
+                }
+            }
+            else if ( fol == "./")
+            {
+                cnt = cnt ;
+            }
+            else
+            {
+                cnt++ ;
+            }
+        }
+
+        return cnt ;
+    }
+};

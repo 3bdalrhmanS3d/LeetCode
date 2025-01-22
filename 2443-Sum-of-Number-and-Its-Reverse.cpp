c431b8eb180d59eddc18742371177c46f853f2b1
Abdalrhman Saad <abdalrhmansaad24@gmail.com>
2025-01-23 00:16:40 +0200

    [2443] [Time Beats: 63.76%] [Memory Beats: 55.05%] - LeetPush

---
+++ 2443-Sum-of-Number-and-Its-Reverse.cpp
@@ -0,0 +1,28 @@
+class Solution {
+private:
+    int reverse(int n)
+    {
+        int ans = 0 ;
+        while(n!=0)
+        {
+            int digit = n % 10 ;
+            ans = ( ans * 10) + digit ;
+            n /= 10 ;
+        }
+
+        return ans;
+    }
+public:
+    bool sumOfNumberAndReverse(int num) {
+        for(int i =0; i<=num ;i++)
+        {
+            
+            if( i + reverse(i) == num)
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+};
